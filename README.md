5cf1e0fe6bf75c2a9e507fc4dda6cb4b4f8fc885
Vitor Leonardo Pauloski <vitorleonardo.pauloski@gmail.com>
2021-01-18 16:46:00 -0300

    setup.py and README.md updated

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # TelePython
-Um pacote para facilitar o envio de mensagens de texto e documentos pelo Telegram
+Um pacote para facilitar o envio de mensagens de texto e documentos pelo Telegram.
--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open('README.md', 'r', encoding = 'utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="telegram-vitorpauloski",
-    version="0.0.1",
-    author="Vitor Leonardo Pauloski",
-    author_email="vitorleonardo.pauloski@gmail.com",
-    description="Um pacote para facilitar o envio de texto e documentos pelo Telegram",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/vitorpauloski/telegram",
-    packages=setuptools.find_packages(),
-    licence='MIT',
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Communications :: Chat",
+    name = 'TelePython',
+    version = '1.0.0',
+    author = 'Vitor Leonardo Pauloski',
+    author_email = 'vitorleonardo.pauloski@gmail.com',
+    description = 'Um pacote para facilitar o envio de mensagens de texto e documentos pelo Telegram.',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/vitorpauloski/TelePython',
+    packages = ['telegram'],
+    licence = 'MIT',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Communications :: Chat',
     ],
-    python_requires='>=3.6',
-    install_requires=['requests'],
+    python_requires = '>=3.8',
+    install_requires = ['requests>=2.25.1'],
 )
