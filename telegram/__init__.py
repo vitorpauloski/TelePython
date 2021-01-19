f8d0eef5b2dd279955ee4b7cf1b36167d655a5c0
Vitor Leonardo Pauloski <vitorleonardo.pauloski@gmail.com>
2021-01-19 14:00:45 -0300

    requirements.txt added and functions return format changed to dict

--- telegram/__init__.py
+++ telegram/__init__.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class Bot:
     def __init__(self, token):
@@ -7,16 +8,16 @@ class Bot:
     def getUpdates(self):
         url = 'https://api.telegram.org/' + self.token + '/getUpdates'
         response = requests.request('GET', url)
-        return response.text
+        return json.loads(response.text)
     
     def sendMessage(self, chat_id, text):
         url = 'https://api.telegram.org/' + self.token + '/sendMessage'
         params = (('chat_id', chat_id),('text', text))
         response = requests.request('GET', url, params = params)
-        return response.text
+        return json.loads(response.text)
 
     def sendDocument(self, chat_id, file_path):
         url = 'https://api.telegram.org/' + self.token + '/sendDocument?chat_id=' + chat_id
         files = {'document': open(file_path, 'rb')}
         response = requests.request('POST', url, files = files)
-        return response.text
+        return json.loads(response.text)
