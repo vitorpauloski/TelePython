8514e63d877756c085740f7fcb6edfad1120c7d8
Vitor Leonardo Pauloski <vitorleonardo.pauloski@gmail.com>
2021-01-26 14:12:49 -0300

    Response object implemented

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ TelePython officially supports Python 3.8 or above.
 >>> import telegram
 >>> bot = telegram.Bot('YOUR_BOT_TOKEN')
 >>> bot.sendMessage('CHAT_ID', 'Your message here!')
+<Response [Success]>
 ```
 
 To learn how to create a Bot and generate an authorization token, read the *BotFather* session from [Telegram Bots documentation](https://core.telegram.org/bots#6-botfather).
@@ -30,3 +31,25 @@ Use this method to send text messages.
 
 - **sendDocument(`chat_id`, `file_path`)**\
 Use this method to send general files. Bots can currently send files of any type of up to 50 MB in size.
+
+## Response object
+
+The Response objects have two parameters:
+
+- **success**\
+Indicates whether the request was successful (True or False).
+
+- **response**\
+returns the result of the request to the Telegram Bot API
+
+Example:
+
+```python
+>>>  import telegram
+>>> bot = telegram.Bot('YOUR_BOT_TOKEN')
+>>> response = bot.getUpdates()
+>>> response.succes
+True
+>>> response.response
+[{'update_id': 675682626, 'message': {'message_id': 56, 'from': {'id': 8644990718, 'is_bot': False, 'first_name': 'Vitor', 'last_name': 'Pauloski', 'username': 'vitorpauloski', 'language_code': 'pt-br'}, 'chat': {'id': 8644990718, 'first_name': 'Vitor', 'last_name': 'Pauloski', 'username': 'vitorpauloski', 'type': 'private'}, 'date': 1611617371, 'text': 'Test Message.'}}]
+```
--- telegram/__init__.py
+++ telegram/__init__.py
@@ -1,23 +1,54 @@
 import requests
 import json
 
+class Response:
+    def __init__(self, success, response):
+        self.success = success
+        self.response = response
+
+    def __repr__(self):
+        if self.success:
+            return '<Response [Success]>'
+        else:
+            return '<Response [Failure]>'
+
 class Bot:
     def __init__(self, token):
         self.token = token
+        self.id = token.split(':')[0].replace('bot', '')
+
+    def __repr__(self):
+        return f'<Bot [{self.id}]>'
 
     def getUpdates(self):
         url = 'https://api.telegram.org/' + self.token + '/getUpdates'
-        response = requests.request('GET', url)
-        return json.loads(response.text)
-    
+        request = requests.get(url)
+        if request.status_code == 200:
+            response = json.loads(request.text)['result']
+            return Response(True, response)
+        else:
+            response = f'Error {request.status_code}.'
+        return Response(False, response)
+
     def sendMessage(self, chat_id, text):
         url = 'https://api.telegram.org/' + self.token + '/sendMessage'
-        params = (('chat_id', chat_id),('text', text))
-        response = requests.request('GET', url, params = params)
-        return json.loads(response.text)
+        params = {'chat_id': chat_id, 'text': text}
+        request = requests.get(url, params = params)
+        if request.status_code == 200:
+            response = json.loads(request.text)['result']
+            return Response(True, response)
+        else:
+            response = f'Error {request.status_code}.'
+            return Response(False, response)
 
     def sendDocument(self, chat_id, file_path):
-        url = 'https://api.telegram.org/' + self.token + '/sendDocument?chat_id=' + chat_id
-        files = {'document': open(file_path, 'rb')}
-        response = requests.request('POST', url, files = files)
-        return json.loads(response.text)
+        url = 'https://api.telegram.org/' + self.token + '/sendDocument'
+        params = {'chat_id': chat_id}
+        files = {'document': open(file_path)}
+        request = requests.post(url, params = params, files = files)
+        if request.status_code == 200:
+            response = json.loads(request.text)['result']
+            return Response(True, response)
+        else:
+            response = f'Error {request.status_code}.'
+            return Response(False, response)
